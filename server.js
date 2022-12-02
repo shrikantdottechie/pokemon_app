5c18aadb45a009dd4f9c3764ee075ab237097e12
shrikantdottechie <106555771+shrikantdottechie@users.noreply.github.com>
2022-12-02 15:13:35 -0500

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -12,11 +12,16 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 });
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
+});
+/*
 app.get('/pokemon/', (req, res) => {
     //res.send(fruits);
     //res.render('Show');
     res.render('Index',{pokemon: pokemon});
-});
+});*/
 
 
 //New - get a form to create a new record
